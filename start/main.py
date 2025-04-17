75f6898c100fc261b52159ed47c8d68d3493e93d
swapnilgarg7 <swapnilgarg810@gmail.com>
2025-04-17 20:50:55 +0530

    webhook setup

--- start/main.py
+++ start/main.py
@@ -9,7 +9,7 @@ import asyncio
 # Load environment variables
 # --------------------------------------------------------------
 
-load_dotenv()
+load_dotenv(override=True)
 ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
 RECIPIENT_WAID = os.getenv("RECIPIENT_WAID")
 PHONE_NUMBER_ID = os.getenv("PHONE_NUMBER_ID")
@@ -41,30 +41,3 @@ response = requests.post(url, headers=headers, json=data)
 
 print("Status Code:", response.status_code)
 print("Response Body:", response.json())
-
-
-# --------------------------------------------------------------
-# Send a template WhatsApp message
-# --------------------------------------------------------------
-
-
-def send_whatsapp_message():
-    url = f"https://graph.facebook.com/{VERSION}/{PHONE_NUMBER_ID}/messages"
-    headers = {
-        "Authorization": "Bearer " + ACCESS_TOKEN,
-        "Content-Type": "application/json",
-    }
-    data = {
-        "messaging_product": "whatsapp",
-        "to": RECIPIENT_WAID,
-        "type": "template",
-        "template": {"name": "hello_world", "language": {"code": "en_US"}},
-    }
-    response = requests.post(url, headers=headers, json=data)
-    return response
-
-
-# Call the function
-response = send_whatsapp_message()
-print(response.status_code)
-print(response.json())
